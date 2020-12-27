a04a5016f47d730533c855efb05d162cee5be5ca
taxintt <nthouse14@gmail.com>
2020-12-27 14:33:31 +0900

    First commit

---
+++ provider.tf
@@ -0,0 +1,27 @@
+provider "google" {
+  version = "3.51.0"
+  project = "${var.project_name}"
+  region  = "${var.region}"
+}
+
+provider "google-beta" {
+  version = "3.51.0"
+  project = "${var.project_name}"
+  region  = "${var.region}"
+}
+
+provider "kubernetes" {
+  # https://github.com/hashicorp/terraform-provider-kubernetes/releases
+  version          = "1.13.3"
+  project          = "${var.project_name}"
+  load_config_file = false
+
+  host  = "https://${data.google_container_cluster.primary.endpoint}"
+  token = "${data.google_client_config.current.access_token}"
+
+  # client_certificate = "${base64decode(google_container_cluster.primary.master_auth.0.client_certificate)}"
+  # client_key = "${base64decode(google_container_cluster.primary.master_auth.0.client_key)}"
+  cluster_ca_certificate = "${base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)}"
+}
+
+
