8efd606ac0d847ae8c8e4283a97cd1ae0b9972ae
taxintt <nthouse14@gmail.com>
2020-12-31 22:56:46 +0900

    Add firewall rules of IAP

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 # https://istio.io/latest/docs/setup/platform-setup/gke/
 resource "google_compute_firewall" "default" {
-  name    = "fw-for-istio"
+  name    = "allow-connections-for-istio"
   network = var.network
 
   allow {
@@ -10,4 +10,16 @@ resource "google_compute_firewall" "default" {
 
   # https://stackoverflow.com/questions/65297738/how-to-use-nodeport-in-custom-kubenetes-cluster-on-gcp
   target_tags = ["istio"]
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "allow-iap-forwarding-ssh"
+  network = var.network
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["35.235.240.0/20"]
 }
