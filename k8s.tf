12cec61a555167967ac21f14fc69aa9ab04a1595
taxintt <nthouse14@gmail.com>
2021-01-03 14:20:34 +0900

    revert

--- k8s.tf
+++ k8s.tf
@@ -56,7 +56,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   # https://blog.yukirii.dev/create-gke-with-least-privilege-sa-using-terraform/
   node_config {
     preemptible     = true
-    machine_type    = "e2-medium-testabc"
+    machine_type    = "e2-medium"
     service_account = "least-privilege-sa-for-gke@${var.project_id}.iam.gserviceaccount.com"
 
     metadata = {
